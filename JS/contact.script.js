165d55a22b3de3a37fa39ad4148b7f616dcc3ef2
Kevin <kevin.bouaziz1@gmail.com>
2021-03-26 10:49:26 +0100

    traduction formulaire

--- JS/contact.script.js
+++ JS/contact.script.js
@@ -1,58 +1,41 @@
-/*$('contactForm').submit(function(e) {
-    let nameFirst = document.getElementById('firstName').value;
-    let nameLast = document.getElementById('lastName').value;
-    let choixSujet = document.getElementById('choix').value;
-    let email = document.getElementById('mail').value;
-    let msg = document.getElementById('message').value;
-    let error = document.getElementById('errorMessage');
-
-    if (!nameFirst.value || !nameLast.value || !email.value || !message.value || choixSujet.value) === {
-        alert('Informations manquantes');
-    } else {
-        e.preventDefault();
-        $(this).get(0).reset();
-        alert('Envoi réussi');
-    }
-});*/
-
 function validation() {
-    let nameFirst = document.getElementById('firstName').value;
-    let nameLast = document.getElementById('lastName').value;
-    let choixSujet = document.getElementById('choix').value;
-    let email = document.getElementById('mail').value;
-    let msg = document.getElementById('message').value;
-    let error = document.getElementById('errorMessage');
+  let nameFirst = document.getElementById("firstName").value;
+  let nameLast = document.getElementById("lastName").value;
+  let choixSujet = document.getElementById("choix").value;
+  let email = document.getElementById("mail").value;
+  let msg = document.getElementById("message").value;
+  let error = document.getElementById("errorMessage");
 
-    error.style.padding = "10px";
-    let text;
-    if (nameFirst.length < 2) {
-        text = "Please Enter valid Name";
-        error.innerHTML = text;
-        return false;
-    }
-    if (nameLast.length < 2) {
-        text = "Please Enter valid Name";
-        error.innerHTML = text;
-        return false;
-    }
+  error.style.padding = "10px";
+  let text;
+  if (nameFirst.length < 2) {
+    text = "Il nous faut un PRÉNOM valide";
+    error.innerHTML = text;
+    return false;
+  }
+  if (nameLast.length < 2) {
+    text = "Il nous faut un NOM valide";
+    error.innerHTML = text;
+    return false;
+  }
 
-    if (choixSujet === "") {
-        text = "Choissisez un sujet";
-        error.innerHTML = text;
-        return false;
-    }
+  if (choixSujet === "") {
+    text = "Choisi un sujet";
+    error.innerHTML = text;
+    return false;
+  }
 
-    if (email.indexOf("@") == -1 || email.length < 6) {
-        text = "Please Enter valid Email";
-        error.innerHTML = text;
-        return false;
-    }
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Renseigne ton adresse Mail";
+    error.innerHTML = text;
+    return false;
+  }
 
-    if (msg.length <= 30) {
-        text = "Please Enter More Than 30 Characters";
-        error.innerHTML = text;
-        return false;
-    }
-    alert("Message envoyé!");
-    return true;
+  if (msg.length <= 5) {
+    text = "Oublie pas d'écrire ton message";
+    error.innerHTML = text;
+    return false;
+  }
+  alert("Message envoyé! Notre équipe va prendre note ton message");
+  return true;
 }
